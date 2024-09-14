8aaf204198de144b45997ce30c318fc7cfe34e4b
NVSYX <nguyensy23112311@gmail.com>
2024-09-14 15:03:27 +0700

    Config BE

---
+++ AI-Enhanced IELTS/IRepository/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRepository
+{
+    public interface IAuthRepository
+    {
+        Task<string> Register(RegistrationRequestModel registrationRequestModel);
+        Task<LoginReponseModel> Login(LoginRequestModel loginRequestModel);
+        Task<bool> AssignRole(string email, string roleName);
+    }
+}
