3bc7ee75132e89b088994950426b31e01b4bac5c
NVSYX <nguyensy23112311@gmail.com>
2024-09-19 23:41:43 +0700

    Push again CLASS ENTITY

--- Entity/Class.cs
+++ Entity/Class.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -21,3 +20,4 @@ namespace Entity
         public string ImageUrl { get; set; }
     }
 }
+
