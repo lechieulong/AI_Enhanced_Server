073328292cebe739b9d55fa9f7d65d0d2b1d9b01
lechieulong <98459887+lechieulong@users.noreply.github.com>
2024-11-24 18:36:12 +0700

    Fix testForeignKey

--- Entity/Migrations/20241124051411_AddAverageCourseRating.cs
+++ Entity/Migrations/20241124051411_AddAverageCourseRating.cs
@@ -23,33 +23,11 @@ namespace Entity.Migrations
                 type: "int",
                 nullable: false,
                 defaultValue: 0);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_TestExams_SectionCourseId",
-                table: "TestExams",
-                column: "SectionCourseId",
-                unique: true);
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_TestExams_CourseParts_SectionCourseId",
-                table: "TestExams",
-                column: "SectionCourseId",
-                principalTable: "CourseParts",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_TestExams_CourseParts_SectionCourseId",
-                table: "TestExams");
-
-            migrationBuilder.DropIndex(
-                name: "IX_TestExams_SectionCourseId",
-                table: "TestExams");
-
             migrationBuilder.DropColumn(
                 name: "AverageRating",
                 table: "Courses");
