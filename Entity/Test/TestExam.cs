b357d0cc83eb3d84248bac92ca3c8d4c4630699b
lechieulong <98459887+lechieulong@users.noreply.github.com>
2024-12-03 11:31:54 +0700

    add courseId to test

--- Entity/Test/TestExam.cs
+++ Entity/Test/TestExam.cs
@@ -10,8 +10,8 @@
         public DateTime CreateAt { get; set; }
         public DateTime UpdateAt { get; set; }
         public Guid LessonId { get; set; }
-        public Guid ClassId { get; set; }
         public Guid SkillIdCourse { get; set; }
+        public Guid CourseId { get; set; }
         public Guid UserID { get; set; }
         public ICollection<Skill> SkillTests { get; set; } = new List<Skill>();
         public int TestCreateBy { get; set; }
