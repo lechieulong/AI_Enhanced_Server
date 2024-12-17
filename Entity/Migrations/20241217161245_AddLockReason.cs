6aa1ad193bdcacae1ceee2ae3fcf9e6c57b114e5
NVSYX <nguyensy23112311@gmail.com>
2024-12-17 23:32:17 +0700

    Change lock user entity.

---
+++ Entity/Migrations/20241217161245_AddLockReason.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLockReason : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "LockoutForever",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LockoutReason",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LockoutForever",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutReason",
+                table: "AspNetUsers");
+        }
+    }
+}
