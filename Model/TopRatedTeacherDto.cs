0daad3a3683ba17698968f9c82a1257968efa78c
NVSYX <nguyensy23112311@gmail.com>
2024-12-20 13:23:11 +0700

    Update rating.

--- Model/TopRatedTeacherDto.cs
+++ Model/TopRatedTeacherDto.cs
@@ -9,6 +9,7 @@ namespace Model
     public class TopRatedTeacherDto
     {
         public string UserName { get; set; }
+        public string FullName { get; set; }
         public string? ImageURL { get; set; }
         public double AverageRating { get; set; }
         public int RatingCount { get; set; }
